1882b82577e9db53e06fed4aa72dac4d9c66bfb8
BERNARD Damien <damien.bernard@epitech.eu>
2020-03-29 21:05:35 +0200

    [FIX] #GH ACTION# Fixing script github to blih.

--- .github/workflows/GithubToBlih.sh
+++ .github/workflows/GithubToBlih.sh
@@ -13,7 +13,7 @@ elif [[ $# == 1 ]]; then
     USER_GHUB="$USER_GHUB"
     USER_BLIH="$USER_BLIH"
     REPO_BLIH="$1"
-    REPO_GHUB=$REPO_GHUB
+    REPO_GHUB="$REPO_BLIH"
 else
     echo "Usage: $0 REPO_NAME"
     exit 1
@@ -21,19 +21,10 @@ fi
 
 
 set -xe
+
+git clone --bare git@github.com:$USER_GHUB/$REPO_GHUB $REPO_GHUB
+cd $TMP_DIR
 git push --mirror git@git.epitech.eu:$USER_BLIH/$REPO_BLIH
 
-
-#TMP_DIR="./temporary_repository"
-#set -xe
-#OLDPWD=$(pwd)
-
-#rm -rf $TMP_DIR
-#mkdir $TMP_DIR
-
-#git clone --bare git@github.com:$USER_GHUB/$REPO_GHUB $TMP_DIR
-#cd $TMP_DIR
-#git push --mirror git@git.epitech.eu:$USER_BLIH/$REPO_BLIH
-#
-#cd $OLDPWD
-#rm -rf $TMP_DIR
+cd $OLDPWD
+rm -rf $REPO_GHUB
