9768620b3cbc10da24a83ab904f13c0c34169f4b
BERNARD Damien <damien.bernard@epitech.eu>
2020-03-29 17:05:57 +0200

    [FIX] Fixing formulas.

--- Ducks.py
+++ Ducks.py
@@ -18,25 +18,25 @@ def frange(start: float, end: float = None, inc: float = 1.0):
 
 
 class Duck:
-    max = 1000
+    max = 100
 
-    def __init__(self, a: float, max_interval: int = max, test: bool = False):
+    def __init__(self, a: float, test: bool = False):
         self.a = a
-        self.interval = frange(0, max_interval / 10, 0.001)
+        self.interval = frange(0,  Duck.max, 0.001)
         self.esp = 0
         self.std_dev = 0
         self.test = test
 
     @staticmethod
     def percent_back(const: float, time_snd: int):
-        return sum(Duck.probability_density(const, i / Duck.max) for i in range(time_snd * Duck.max)) / 10
+        return sum(10 * Duck.probability_density(const, i) for i in frange(0, time_snd, 0.001)) / 100
 
     @staticmethod
     def time_back(a: float, p: float):
         res = 0
         for t in frange(0, Duck.max, 0.01):
             res += Duck.probability_density(a, t)
-            if res >= p:
+            if res > p:
                 return t
         raise ValueError
 
